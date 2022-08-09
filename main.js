b07aa852d5843829e79d0389ed57aa341a3d7e19
Temirrlan98 <dastanovtim@gmail.com>
2022-08-09 20:04:13 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,161 @@
+// console.log("Hello JS!");
+/*console.error("Hello JS!");
+console.warn("Hello JS!");*/
+// ? переьенные
+// let, var ,const
+
+// console.log(birth);
+// const birth = "08.08.1998";
+// console.log(birth);
+// birth = "08.08.1999";
+
+// console.log(lastName);
+/*let lastName = "Pavlova";
+lastName = "Pirogovo";
+console.log(lastName);*/
+
+// console.log(firsName);
+// var firsName = "tima";
+// console.log(firsName);
+// firsName = "tomo";
+// console.log(firsName);
+
+// ! название переменных
+// нелзя ставить цифру в начало
+// let 5blog = ''
+// зарезе
+// let let
+// let fisrt Name
+// let fisrt-Name
+// const Red= "abba"
+
+// ? так можно
+// let camelCase
+// let color_orange
+// const RED = "#qww1313"
+// const $blog = "";
+// const number10 =10
+
+// ! типы данных
+// 1 string
+// let str = "strochka";
+// console.log(str.length);
+// console.log(str);
+// const typeOfStr = typeof str;
+// console.log(typeof str);
+
+// let str1 ="dwe";
+// let str2 = "asd";
+// let str = `zxz`;
+
+// 2 number
+
+// let num1 = 100;
+// const PI = 3.14;
+// console.log(typeof num1);
+
+// 3 Boolean
+// const bool1 = true;
+// const bool2 = false;
+
+// 4 null-специальная пустота
+// let error=null
+
+// 5 undefined
+// let Hello;
+// console.log(Hello);
+
+// 6 Object(object, array)
+// let user = { name: "diana Kim", age: 20 };
+// console.log(user, name);
+// console.log(user, age);
+
+// console.log(user["name"]);
+
+// Array
+// let arr = [false, 100, true, {}, [], "hello"];
+// console.log(arr.length);
+// console.log(arr);
+// console.log(arr[0]);
+// console.log(arr[5]);
+
+// 7 symbol
+// let id= Symbol("hfrubf2")
+
+// 8 BigInt
+// let int = 45345354n
+
+// ! Math operators
+// console.log(1+5)
+// console.log(5-2)
+// console.log(10/2)
+// console.log(10*10)
+// console.log(2 ** 3);
+// console.log(10 % 7);
+// NaN  not a number
+// console.log("hello" / 7);
+
+// ! - / *
+// console.log(10 / 2);
+// +
+// ! concat -> слияние
+// console.log("10" + "2");
+// console.log(10 + "2");
+// console.log("10" + 2);
+
+// ! true-false значение
+
+// console.log(Boolean(0)); false
+// console.log(Boolean(-1)); true
+// console.log(Boolean(undefined));false
+// console.log(Boolean(null));false
+// console.log(Boolean("hello"));true
+// console.log(Boolean());false
+
+// ! сравнение
+// ! строгое(типы данных и значение должны быт ровны)
+// console.log(3 === 5);false;
+// console.log(3 === 3);true;
+// console.log(3 === "3");false;
+// console.log(3 !== 5);true
+// ! нестрогое
+// console.log(3 === 3)true
+// console.log("3" === 3)true
+// console.log(3 !=== 3)false
+// ! < > =
+// console.log(3 < 5);true;
+// console.log(3 > 5);false;
+// console.log(4 >= 4);true;
+// console.log(4 <= 4);true;
+
+// ! из String в Number
+// let elem = "10000";
+// console.log(elem);
+// console.log(+elem);
+
+// ! parseint
+// let elem2 = "10000dthfyhfgchgy";
+// console.log(parseint(elem2));
+
+// ! parseFlot
+// let elem3 = "10.2000dthfyhfgchgy";
+
+// let elem4 = "10000";
+// console.log(Number(elem4));
+
+// let num = "6443";
+// console.log(num);
+
+// alert("hello,user!");
+// let firsName = prompt("как вас зовут?");
+// console.log(firsName);
+// alert(`${firsName}, welcome!`);
+
+// let value = confirm("вам есть 18?");
+// console.log(confirm);
+
+// let num1 = prompt("ведите первое число");
+// let operators = prompt("ведите матем опер");
+// let num2 = prompt("ведите второе число");
+// let result = eval(num1 + operators + num2);
+// alert(result);
